4018f2185c75cfde2571884a24102ed1767b4a12
blessedrajp <blessedrajp@gmail.com>
2025-12-03 21:35:31 +0530

    dummy commit

--- src/main/java/com/example/TTMS/entity/RideTicket.java
+++ src/main/java/com/example/TTMS/entity/RideTicket.java
@@ -7,7 +7,7 @@ import org.springframework.data.annotation.Id;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.NoArgsConstructor; 
 
 @Data
 @AllArgsConstructor
